6b0b51168024d6c6a5fad4043083d9befe8a385e
David Parra <davida.parra10@gmail.com>
2026-04-21 19:36:35 +0000

    redo the splitCSV function

--- Graph.cpp
+++ Graph.cpp
@@ -1,8 +1,11 @@
 #include "Graph.hpp"
+#include "Heap.hpp"
 #include <fstream>
 #include <vector>
 #include <string>
 #include <iostream>
+
+
 
 // Splits line and adds its data to an array
 bool splitCSVLine(const std::string& line, std::string tokens[]) {
@@ -29,7 +32,6 @@ bool splitCSVLine(const std::string& line, std::string tokens[]) {
         }
         col++; // moves to fill next column data
     }
-
     return col == 6; // returns true when all columns where filled for all lines
 }
 
@@ -65,7 +67,7 @@ void Graph::parseAndBuild(std::ifstream& infile) {
 void Graph::addNode(const std::string& code, const std::string& city) {
     int index = findNode(code);
 
-    if(index == -1) {return;}
+    if(index != -1) {return;}
 
     airports.push_back(Airport(code, city));
 }
@@ -82,5 +84,37 @@ void Graph::addEdge(const std::string& from, const std::string& to, int distance
     airports[fromIndex].adjacent.push_back(Flight(toIndex, distance, cost));
 }
 
+// Shortest Possible Path algorith (Dijksra) 
+// Finds all of the Paths and adds them into the MinHeap
+// Minheap sorts for shorest path staying at the root
+// Dijkstra ends by pulling the root from the MinHeap
+std::vector<int> Graph::dijkstra(int source) {
+    // Declaring Infinty as INF for the algo.
+    const int INF = 1e9;
+    std::vector<int> dist(airports.size(), INF);
+    dist[source] = 0;
 
+    MinHeap minHeap;
+    minHeap.push(0, source);
+
+    while(!minHeap.empty()) {
+        HeapNode current = minHeap.pop();
+        int currentScore = current.combined_score;
+        int currentNode = current.node;
+
+        if(currentScore > dist[currentNode]) continue;
+
+        for (const Flight& flight : airports[currentNode].adjacent){
+            int combined = flight.distance + flight.cost;
+            int newDist =  dist[currentNode] + combined;
+
+            if (newDist < dist[flight.destination]) {
+                dist[flight.destination] = newDist;
+                minHeap.push(newDist, flight.destination);
+            }
+        }
+    }
+
+    return dist;
+}
 
