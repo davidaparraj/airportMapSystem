3213c2b4b692f9f478543cce21ada343db0e5e04
Matthew-R-P <mpollock8587@floridapoly.edu>
2026-04-21 19:23:03 +0000

    added the dijkstra algorithm

--- Graph.cpp
+++ Graph.cpp
@@ -1,13 +1,15 @@
 #include "Graph.hpp"
+#include "Heap.hpp"
 #include <fstream>
 #include <vector>
 #include <string>
 #include <iostream>
 
+
+
 // TODO ----> FINISH LOGIC. Splits values and stores them in tokens[]
 bool splitCSVLine(const std::string& line, std::string tokens[]) {
     bool inQuotes  = false;
-
 
     return inQuotes;
 }
@@ -44,7 +46,7 @@ void Graph::parseAndBuild(std::ifstream& infile) {
 void Graph::addNode(const std::string& code, const std::string& city) {
     int index = findNode(code);
 
-    if(index == -1) {return;}
+    if(index != -1) {return;}
 
     airports.push_back(Airport(code, city));
 }
@@ -61,5 +63,37 @@ void Graph::addEdge(const std::string& from, const std::string& to, int distance
     airports[fromIndex].adjacent.push_back(Flight(toIndex, distance, cost));
 }
 
+// Shortest Possible Path algorith (Dijksra) 
+// Finds all of the Paths and adds them into the MinHeap
+// Minheap sorts for shorest path staying at the root
+// Dijkstra ends by pulling the root from the MinHeap
+std::vector<int> Graph::dijkstra(int source) {
+    // Declaring Infinty as INF for the algo.
+    const int INF = 1e9;
+    std::vector<int> dist(airports.size(), INF);
+    dist[source] = 0;
 
+    MinHeap minHeap;
+    minHeap.push(0, source);
+
+    while(!minHeap.empty()) {
+        HeapNode current = minHeap.pop();
+        int currentScore = current.combined_score;
+        int currentNode = current.node;
+
+        if(currentScore > dist[currentNode]) continue;
+
+        for (const Flight& flight : airports[currentNode].adjacent){
+            int combined = flight.distance + flight.cost;
+            int newDist =  dist[currentNode] + combined;
+
+            if (newDist < dist[flight.destination]) {
+                dist[flight.destination] = newDist;
+                minHeap.push(newDist, flight.destination);
+            }
+        }
+    }
+
+    return dist;
+}
 
